22b6aed377056082aab2e643fc2c1b75e9a709bc
rabik <rezhake.rebike@citi.com>
2019-01-25 03:20:38 -0600

    Added form module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -28,6 +30,8 @@ import { ContactComponent } from './components/contact/contact.component';
     ],
     imports: [
         BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
         AppRoutingModule,
         SlideshowModule
     ],
