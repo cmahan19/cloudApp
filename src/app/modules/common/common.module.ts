fb4317a7be26a81cbf57e2faabcb46031547f376
rabik <rezhake.rebike@citi.com>
2019-01-26 00:00:35 -0600

    Added common module

---
+++ src/app/modules/common/common.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+    declarations: [
+
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
+    exports: [
+        
+    ]
+})
+export class CommonModule {
+
+}
