902ae52b38ece9a892dcc98708857807c5c7bbed
rabik <rezhake.rebike@citi.com>
2019-01-26 17:41:13 -0600

    Added greetings component to about and index page

--- src/app/main/components/about/about.component.ts
+++ src/app/main/components/about/about.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: "main-about",
-    templateUrl: "./about.component.html",
-    styleUrls: ["./about.component.scss"]
+    selector: 'main-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
 
+    greetingsTitle: string;
+    greetingsText: string;
+    
+    constructor() { }
+
+    ngOnInit() {
+        this.greetingsTitle = 'What We Do'
+        this.greetingsText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quia ut magnam sit natus rem voluptate quo eveniet reprehenderit accusamus!'
+    }
 }
--- src/app/main/components/index/index.component.ts
+++ src/app/main/components/index/index.component.ts
@@ -5,9 +5,15 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: './index.component.html',
     styleUrls: ['./index.component.scss']
 })
-export class IndexComponent implements OnInit{
+export class IndexComponent implements OnInit {
+
+    greetingsTitle: string;
+    greetingsText: string;
+
+    constructor() { }
 
     ngOnInit() {
-        
+        this.greetingsTitle = 'We Want You'
+        this.greetingsText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quia ut magnam sit natus rem voluptate quo eveniet reprehenderit accusamus!'
     }
 }
