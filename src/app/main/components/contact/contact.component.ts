b9a5bdffd9b76c701e10570807dc4ab421050076
rabik <rezhake.rebike@citi.com>
2019-01-26 17:27:09 -0600

    Added greeting component and fixed the style

--- src/app/main/components/contact/contact.component.ts
+++ src/app/main/components/contact/contact.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: "main-contact",
-    templateUrl: "./contact.component.html",
-    styleUrls: ["./contact.component.scss"]
+    selector: 'main-contact',
+    templateUrl: './contact.component.html',
+    styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+
+    greetingsTitle: string;
+    greetingsText: string;
     
     constructor() { }
 
     ngOnInit() {
-
+        this.greetingsTitle = 'Get In Touch'
+        this.greetingsText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quia ut magnam sit natus rem voluptate quo eveniet reprehenderit accusamus!'
     }
 }
