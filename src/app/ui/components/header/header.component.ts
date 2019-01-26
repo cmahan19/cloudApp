f36ea4abe9d4f43caf0bd3a478fa80a399395558
rabik <rezhake.rebike@citi.com>
2019-01-26 15:06:41 -0600

    Added breadcrumbs to header component

--- src/app/ui/components/header/header.component.ts
+++ src/app/ui/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 import { ActivatedRoute, UrlSegment } from '@angular/router';
 
 @Component({
@@ -8,13 +8,26 @@ import { ActivatedRoute, UrlSegment } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
+    @Input("header") header: string;
     currPages: string[];
+    breadcrumbs: string[] = [];
 
     constructor(private _route: ActivatedRoute) { }
 
     ngOnInit() {
         this._route.url.subscribe((routes: UrlSegment[]) => {
             this.currPages = routes.map(route => route.path);
+            this.header = this.header? this.header : routes.pop().toString();
+            this.setBreadcrumbs(this.currPages);
         })
+    }
+
+    setBreadcrumbs(currPages: string[]) {
+        let link = currPages[0];
+        this.breadcrumbs.push(link);
+        for (let i = 1; i < currPages.length; i++) {
+            link += '/' + currPages[i];
+            this.breadcrumbs.push(link);
+        }
     }
 }
