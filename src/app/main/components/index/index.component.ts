f2276c02a8bd4e4aa740c9dfa163412410c06fc3
rabik <rezhake.rebike@citi.com>
2019-02-03 03:03:43 -0600

    Changed the icon names

--- src/app/main/components/index/index.component.ts
+++ src/app/main/components/index/index.component.ts
@@ -19,37 +19,37 @@ export class IndexComponent implements OnInit {
         this.greetingsText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quia ut magnam sit natus rem voluptate quo eveniet reprehenderit accusamus!'
         this.featureCards = [
             {
-                icon: 'cogs',
+                icon: 'auto',
                 title: 'Automate Business Process',
                 text: 'Streamline as many of your business processes as possible. For simplicity, increase service quality, improve service delivery and lower costs.',
                 url: '/'
             },
             {
-                icon: 'cogs',
+                icon: 'software',
                 title: 'Mobile and Desktop Applications',
                 text: 'Apps make you visible, provide a marketing platform, automate processes, display data visualizations, build recognition, add value, make your business stand out, cultivate employee and customer engagement.',
                 url: '/'
             },
             {
-                icon: 'cogs',
+                icon: 'cloud',
                 title: 'Custom Cloud Solutions',
                 text: 'Cloud computing can help you do more with less. Our services will reduce your overhead and help drive your business to new levels.',
                 url: '/'
             },
             {
-                icon: 'cogs',
+                icon: 'data',
                 title: 'Data Analytics and Visualizations',
                 text: 'Get critical insights in real time with data storage, warehousing, visualize analytics, and machine learning.',
                 url: '/'
             },
             {
-                icon: 'cogs',
+                icon: 'it',
                 title: 'Modernize IT Infrastructure',
                 text: 'Leverage technology to meet your business goals. Improve business experiences. Modernization will',
                 url: '/'
             },
             {
-                icon: 'cogs',
+                icon: 'website',
                 title: 'Website Design',
                 text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur nemo ducimus iste eligendi quidem enim.',
                 url: '/'
